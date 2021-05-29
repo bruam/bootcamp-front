4bf526d822f07845fe3849789579f3a1dbc5f30c
Bruno Martins <brunoagmart@gmail.com>
2021-05-29 18:20:19 -0300

    primeira versão do front-end

---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,12 @@
+class Stock {
+  id: number;
+  name: string;
+  price: number;
+  minimum: 0;
+  exclusiveMinimum: boolean;
+  variation: number;
+  date: string;
+  list: string[];
+}
+
+export default Stock;
